65770f0580760feb28e9e54409f80a50c50bb239
DRTX2 <davidmanjarres2004@gmail.com>
2025-04-23 00:03:14 -0500

    Edit transfer service

--- src/main/java/com/drtx/bank/controller/TransferController.java
+++ src/main/java/com/drtx/bank/controller/TransferController.java
@@ -1,16 +1,27 @@
 package com.drtx.bank.controller;
 
+
+import com.drtx.bank.command.model.TransferMoneyCommand;
 import com.drtx.bank.service.TransferService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transfer")
-
 public class TransferController {
+
     private final TransferService transferService;
     public TransferController(TransferService transferService){
         this.transferService=transferService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> transfer(@RequestBody TransferMoneyCommand command) {
+        transferService.transfer(command.getFromClientId(), command.getToClientId(), command.getAmount());
+        return ResponseEntity.ok("Transferencia realizada con éxito");
     }
 
 }
--- src/main/java/com/drtx/bank/service/TransferService.java
+++ src/main/java/com/drtx/bank/service/TransferService.java
@@ -1,13 +1,56 @@
 package com.drtx.bank.service;
 
+import com.drtx.bank.entity.Client;
+import com.drtx.bank.entity.Transfer;
 import com.drtx.bank.repository.ClientRepository;
+import com.drtx.bank.repository.TransferRepository;
+import jakarta.transaction.Transactional;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@NoArgsConstructor
-public class TransferService {
+import java.time.LocalDateTime;
 
-    private ClientRepository clientRepository;
-    
+@Service
+public class TransferService {
+    private final ClientRepository clientRepository;
+    private final TransferRepository transferRepository;
+
+    public TransferService(ClientRepository clientRepository, TransferRepository transferRepository) {
+        this.clientRepository = clientRepository;
+        this.transferRepository = transferRepository;
+    }
+
+    @Transactional
+    public void transfer(Long fromClientId, Long toClientId, Double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        }
+
+        Client from = clientRepository.findById(fromClientId)
+                .orElseThrow(() -> new RuntimeException("Origin client does not exists with ID "+fromClientId));
+
+        Client to = clientRepository.findById(toClientId)
+                .orElseThrow(() -> new RuntimeException("Arrive client does not exists with ID "+fromClientId));
+
+        if (from.getBalance() < amount) {
+            throw new RuntimeException("");
+        }
+
+        // Actualizamos los saldos
+        from.setBalance(from.getBalance() - amount);
+        to.setBalance(to.getBalance() + amount);
+
+        // Guardamos los cambios en los clientes
+        clientRepository.save(from);
+        clientRepository.save(to);
+
+        // Registramos la transferencia
+        Transfer transfer = new Transfer();
+        transfer.setFromClient(from);
+        transfer.setToClient(to);
+        transfer.setAmount(amount);
+        transfer.setTimestamp(LocalDateTime.now());
+
+        transferRepository.save(transfer);
+    }
 }
